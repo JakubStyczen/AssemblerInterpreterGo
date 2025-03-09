dc48ec8579876432e7379919e3ea8d0768f6c567
JakubStyczen <66252957+JakubStyczen@users.noreply.github.com>
2025-03-09 21:35:48 +0100

    Added missing docs

--- instruction_parser.go
+++ instruction_parser.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+// Parses the argument of an instruction
+// If the argument is a register, it returns the value of the register
+// If the argument is a number, it returns the number
+// If the argument is not a number or a register, it returns 0
 func ParseArg(arg string, reg map[string]int) int {
 	if val, ok := reg[arg]; ok {
 		return val
@@ -29,6 +33,8 @@ func filterEmptyLinesAndComments(program_lines []string) []string {
 	return filter_comment_lines
 }
 
+// Parses the labels of the program
+// Returns a map with the label as key and the index of the instruction as value
 func ParseLabels(program_instruction_list []string) map[string]int {
 	subroutines := make(map[string]int)
 	for i, line := range program_instruction_list {
@@ -59,6 +65,13 @@ func trim_array(arr []string, delimiter string) []string {
 	return trimed_array
 }
 
+// Prepares the instruction to be executed
+// Returns the instruction and its arguments
+// If the instruction is msg, it returns the instruction and the arguments
+// separated by commas
+// If the instruction is a label, it returns the instruction and an empty array
+// If the instruction is a 2 arguments instruction, it returns the instruction and the arguments
+// If the instruction is a 3 arguments instruction, it returns the instruction and the arguments
 func PrepareInstruction(instruction string) (string, []string) {
 	instruction_parts := strings.Split(instruction, " ")
 	if instruction_parts[0] == "msg" {
